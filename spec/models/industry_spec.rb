324d0694e10141b746984cfd6bf93cae3794b0f2
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-02 22:23:42 +0500

    Create tests for Industries

---
+++ spec/models/industry_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Industry, type: :model do
+  let(:industry) { Industry.create(:name => "Automotive") }
+  let(:car) { Car.create(:name => "Ford", :model => "Focus", :engine => "1.6", :image => "image.jpg", :price => "100", :seats => "5", :year => "2020", :industry_id => industry.id) }
+
+  describe "validations" do
+    context "when all attributes are valid" do
+      it "is valid" do
+        expect(industry).to be_valid
+      end
+    end
+  end
+
+  describe "associations" do
+    context "when car is associated" do
+      it "is associated" do
+        expect(industry.cars).to eq([car])
+      end
+    end
+  end
+end
