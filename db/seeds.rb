5adfc5d6c2de4486be7cc50b8db02569035bd3ef
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-05 02:53:48 +0500

    Update seeds car create

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 user = User.create(name: "Bob", email: "bob@gmail.com", password: "123456", password_confirmation: "123456")
 
-Create.create(model: "BMW", name: "X5", engine: "V8", seats: 5, price: 100000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw6_yuup7a.webp", user_id: user.id)
-Create.create(model: "BMW", name: "M3", engine: "V8", seats: 4, price: 147000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw2_atzaxu.webp", user_id: user.id)
-Create.create(model: "BMW", name: "M5", engine: "V8", seats: 5, price: 170000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw5_qzflle.webp", user_id: user.id)
-Create.create(model: "BMW", name: "X6", engine: "V8", seats: 5, price: 200000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw4_v4hc9h.webp", user_id: user.id)
-Create.create(model: "BMW", name: "X1", engine: "V8", seats: 5, price: 150000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw3_szyvlw.webp", user_id: user.id)
-Create.create(model: "BMW", name: "X3", engine: "V8", seats: 5, price: 130000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw1_qiyp6o.webp", user_id: user.id)
+Car.create(model: "BMW", name: "X5", engine: "V8", seats: 5, price: 100000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw6_yuup7a.webp", user_id: user.id)
+Car.create(model: "BMW", name: "M3", engine: "V8", seats: 4, price: 147000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw2_atzaxu.webp", user_id: user.id)
+Car.create(model: "BMW", name: "M5", engine: "V8", seats: 5, price: 170000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284347/Cars/BMW/bmw5_qzflle.webp", user_id: user.id)
+Car.create(model: "BMW", name: "X6", engine: "V8", seats: 5, price: 200000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw4_v4hc9h.webp", user_id: user.id)
+Car.create(model: "BMW", name: "X1", engine: "V8", seats: 5, price: 150000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw3_szyvlw.webp", user_id: user.id)
+Car.create(model: "BMW", name: "X3", engine: "V8", seats: 5, price: 130000, image: "https://res.cloudinary.com/dhatgaadw/image/upload/v1643284346/Cars/BMW/bmw1_qiyp6o.webp", user_id: user.id)
