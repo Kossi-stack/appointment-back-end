a073239a272f83d0c2c581d1025f9c179e86022a
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-05 02:42:31 +0500

    linters check

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::CarsController < ApplicationController
-
   def index
     @cars = Car.all
     render json: @cars
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
-          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :cars, dependent: :destroy
   has_many :appointments, dependent: :destroy
